387fe49d8068c7bc87c7df5f0599d6db29a488bd
vagrant <akzouksia@icloud.com>
2020-11-29 14:20:51 +0900

    [Update]user show_view

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b2b69cf46c7ef975eda65fa037073a5b210c5d19737966b935e47210b585ff2f077d7901e5c51dd9f8025a71afa9cb2bf6434b4b9d753385008ab752943ebaa5'
