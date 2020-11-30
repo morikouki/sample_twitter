8d98433a921aec8c0600e4f6192f79c6ae5b1509
vagrant <akzouksia@icloud.com>
2020-11-30 13:29:40 +0900

    [Update]favorite ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,14 @@ class FavoritesController < ApplicationController
 
 
 	def create
-		favorite = current_user.favorites.new(post_id: params[:post_id])
+		@post = Post.find(params[:post_id])
+		favorite = current_user.favorites.new(post_id: @post.id)
 		favorite.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
-		favorite = current_user.favorites.find_by(post_id: params[:post_id])
+		@post = Post.find(params[:post_id])
+		favorite = current_user.favorites.find_by(post_id: @post.id)
 		favorite.destroy
-		redirect_back(fallback_location: root_path)
 	end
 end
