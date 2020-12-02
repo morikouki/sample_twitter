66d94bb06714475fcd173da177056c818a40f9ab
vagrant <akzouksia@icloud.com>
2020-12-02 11:05:25 +0900

    [Update]post_comment model

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,2 +1,14 @@
 class PostCommentsController < ApplicationController
+
+
+	def create
+	end
+
+	def destroy
+	end
+
+	private
+	def post_comment_params
+		params.require(:post_comment).permit(:comment, :user_id, :post_id)
+	end
 end
