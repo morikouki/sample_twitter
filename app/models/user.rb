11a22120bb693adb1a1b30ec90bf678e61b521f2
vagrant <akzouksia@icloud.com>
2020-12-02 12:06:03 +0900

    [Update]user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,21 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
 
-  has_many :followed_relationship, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
-  has_many :followed_user, through: :followed, source: :follower
-
   has_many :follower_relationship, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
-  has_many :follower_user, through: :follower, source: :followed
+  has_many :follower_user, through: :follower_relationship, source: :followed
+
+  has_many :followed_relationship, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followed_user, through: :followed_relationship, source: :follower
 
   def follow(user_id)
-  	followed_relationship.crate(followed_id: user_id)
+  	follower_relationship.create(followed_id: user_id)
   end
 
   def unfollow(user_id)
-  	followed_relationship.find_by(followed_id: user_id).destroy
+  	follower_relationship.find_by(followed_id: user_id).destroy
   end
 
   def following?(user)
-  	followed_user.include?(user)
+  	follower_user.include?(user)
   end
 end
